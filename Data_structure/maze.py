9dd2f444636b49f4532047572a3f9f9ae8497b9d
navill <blue_jihoon@naver.com>
2019-04-11 15:32:17 +0900

    Update maze.py

--- Data_structure/maze.py
+++ Data_structure/maze.py
@@ -1,1 +1,32 @@
-print('a')
+from lstack import LStack
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def empty(self):
+        if not self.head:
+            return True
+        return False
+
+    def add(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        new_node.next = self.head
+        self.head = new_node
+
+    def traverse(self):
+        cur = self.head
+        while cur:
+            yield cur
+            cur = cur.next
+
