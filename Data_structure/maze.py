5ebdf6908bfb1af735cefc4db815f7057fc19e59
navill <blue_jihoon@naver.com>
2019-04-11 17:53:25 +0900

    Update maze.py
    final version

--- Data_structure/maze.py
+++ Data_structure/maze.py
@@ -41,11 +41,10 @@ class Position:
 class MazeSolver:
     direction = ((-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1))
 
-    def __init__(self, maze):  # maze : doubled_list
+    def __init__(self, maze):
         self.maze = maze
-        # EXIT_ROW, EXIT_COL : 출구 행과 열을 나타냄
-        self.EXIT_ROW = len(self.maze)
-        self.EXIT_COL = len(self.maze[0])
+        self.EXIT_ROW = len(maze)
+        self.EXIT_COL = len(maze[0])
 
         for row in maze:
             row.insert(0, 1)
@@ -59,39 +58,34 @@ class MazeSolver:
 
     def get_path(self):
         stack = LStack()
-        # mark : 0으로된 미로 크기의 이중 리스트
         mark = []
+        for _ in range(self.EXIT_ROW + 2):
+            mark.append([0 for _ in range(self.EXIT_COL + 2)])
 
-        row = None  # 현재 위치
-        col = None
-        dir = None
-        next_row = None  # 이동 가능 여부 확인
-        next_col = None
-        found = False  # 찾았을 때, True
+        row = None;
+        col = None;
+        dir = None;
+        next_row = None;
+        next_col = None;
+        found = False
 
-        # 미로의 시작점
         mark[1][1] = 1
-        # 첫 시작 위치 스택에 push
         stack.push(Position(1, 1, 2))
 
-        # stack is empty -> not found final destination
         while not stack.empty() and not found:
-            # 경로를 찾기 전, pop에 의해 모든 스택이 비어있을 경우 갈곳이 있는지 검사
-            #
-            # stack pop -> row, col, dir : current path update
             pos = stack.pop()
             row = pos.row
             col = pos.col
             dir = pos.dir
-            # if search the every direction and not found, while is terminated
+
             while dir < 8 and not found:
                 next_row = row + self.direction[dir][0]
                 next_col = col + self.direction[dir][1]
+
                 if next_row == self.EXIT_ROW and next_col == self.EXIT_COL:
                     found = True
                     stack.push(Position(row, col, dir))
-                    stack.push(Position(next_row, next_col, 0))
-
+                    stack.push(Position(self.EXIT_ROW, self.EXIT_COL, 0))
                 elif self.maze[next_row][next_col] == 0 and mark[next_row][next_col] == 0:
                     mark[next_row][next_col] = 1
                     stack.push(Position(row, col, dir))
@@ -100,8 +94,9 @@ class MazeSolver:
                     dir = 0
                 else:
                     dir += 1
+
         if found:
-            while not stack.empty():  # final destination
+            while not stack.empty():
                 self.path.add(stack.pop())
         else:
             print('There is no path in this maze!')
@@ -167,5 +162,3 @@ if __name__ == "__main__":
     maze_solver.get_path()
     maze_solver.print_path()
     maze_solver.show_path()
-
-print('git test')
